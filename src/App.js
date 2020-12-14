b30d7172e44094a50d306e5548fcc4e0d581d1ab
FourthOf5 <fromphototoa4@gmail.com>
2020-12-14 19:48:41 +0300

    the starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is the app that's gonna push your goals forward.
+      N-not that i c-care about that, baka!...
     </div>
   );
 }
